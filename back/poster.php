2cfdc0b1e1a8abd96e2720f7d25ac278fbc8190b
unknown <tsubasababy1013@gmail.com>
2022-08-08 09:15:09 +0800

    完成後台預告片資料編輯更新功能

--- back/poster.php
+++ back/poster.php
@@ -1,70 +1,77 @@
-<div style="height:300px">
-  <h3 class="ct">預告片清單</h3>
-  <div style="width:95%;display:flex;justify-content:space-between">
+<div style="height:300px;">
+<div class="ct">預告片清單</div>
+
+<div style="width:100%;display:flex;justify-content:space-between">
     <div style="width:24.6%;text-align:center;background:#eee">預告片海報</div>
     <div style="width:24.6%;text-align:center;background:#eee">預告片片名</div>
     <div style="width:24.6%;text-align:center;background:#eee">預告片排序</div>
     <div style="width:24.6%;text-align:center;background:#eee">操作</div>
-  </div>
-  <form action="./api/edit_poster.php" method="post">
-    <div style="width:100%;height:200px;overflow:auto">
-      <?php
-      // 由小到大去排序
-      $rows=$Poster->all(" order by rank");
-      foreach($rows as $row){
-      ?>
-      <div style="width:95%;display:flex;justify-content:space-between;margin:2px 0">
-        <!-- 圖片 -->
-        <div style="width:24.6%" class="ct">
-          <img src="./upload/<?=$row['img'];?>" style="height:70px;">
-        </div>
-        <!-- 片名 -->
-        <div style="width:24.6%" class="ct">
-          <input type="text" name="name[]" value="<?=$row['name'];?>">
-        </div>
-        <!-- 上下按鈕 -->
-        <div style="width:24.6%" class="ct">
-          <button type="button">往上</button>
-          <button type="button">往下</button>
-        </div>
-        <!-- 操作 -->
-        <div style="width:24.6%">
-          <input type="checkbox" name="sh[]" value="<?=$row['id'];?>" <?=($row['sh']==1)?'checked':'';?>>顯示
-          <input type="checkbox" name="del[]" value="<?=$row['id'];?>">刪除
-          <!-- 動畫 -->
-          <select name="ani[]" class="ct">
+</div>
+<form action="./api/edit_poster.php" method="post">
+<div style="width:100%;height:210px;overflow:auto">
+<?php
+$rows=$Poster->all(" order by rank");
+foreach($rows as $row){
+?>
+<div style="width:100%;display:flex;justify-content:space-between;margin:2px 0">
+    <div style="width:24.6%" class="ct">
+        <img src="./upload/<?=$row['img'];?>" style="height:70px;">
+    </div>
+    <div style="width:24.6%" class="ct"><input type="text" name="name[]" value="<?=$row['name'];?>"></div>
+    <div style="width:24.6%" class="ct">
+        <button type="button">往上</button>
+        <button type="button">往下</button>
+    </div>
+    <div style="width:24.6%" class="ct">
+        <input type="checkbox" name="sh[]" value="<?=$row['id'];?>" <?=($row['sh']==1)?'checked':'';?>> 顯示
+        <input type="checkbox" name="del[]" value="<?=$row['id'];?>"> 刪除
+        <select name="ani[]">
             <option value="1" <?=($row['ani']==1)?'selected':'';?>>淡入淡出</option>
             <option value="2" <?=($row['ani']==2)?'selected':'';?>>滑入滑出</option>
             <option value="3" <?=($row['ani']==3)?'selected':'';?>>縮放</option>
-          </select>
-          <input type="hidden" name="id[]" value="<?=$row['id'];?>">
-        </div>
-      </div>
-      <?php
-      }
-      ?>
+        </select>
+        <input type="hidden" name="id[]" value="<?=$row['id'];?>">
     </div>
-    <div style="width:100%" class="ct">
-      <input type="submit" value="編輯確定">
-      <input type="reset" value="重置">
-    </div>
-  </form>
 </div>
+
+<?php  
+}
+
+?>
+</div>
+<div style="width:100%;" class="ct">
+<input type="submit" value="編輯確定"><input type="reset" value="重置">
+</div>
+</form>
+
+
+</div>
+
+
+
+
+
+
+
+
+
+
+
 <hr>
-<div style="height:180px">
-  <h3 class="ct">新增預告片海報</h3>
-  <form action="./api/add_poster.php" method="post" enctype="multipart/form-data">
-    <table style="width:80%;margin:auto">
-      <tr>
+<div style="height:180px;">
+<div class="ct">新增預告片海報</div>
+<form action="./api/add_poster.php" method="post" enctype="multipart/form-data">
+<table style="width:80%;margin:auto">
+    <tr>
         <td>預告片海報</td>
         <td><input type="file" name="img" id=""></td>
         <td>預告片片名</td>
         <td><input type="text" name="name" id=""></td>
-      </tr>
-    </table>
-    <div class="ct">
-      <input type="submit" value="新增">
-      <input type="reset" value="重置">
-    </div>
-  </form>
+    </tr>
+</table>
+<div class="ct">
+    <input type="submit" value="新增">
+    <input type="reset" value="重置">
+</div>
+</form>
 </div>
--- base.php
+++ base.php
@@ -1,119 +1,126 @@
 <?php
-date_default_timezone_set('Asia/Taipei');
+date_default_timezone_set("Asia/Taipei");
 session_start();
 
-class DB{
-  protected $dsn="mysql:host=localhost;charset=utf8;dbname=db03";
-  protected $user='root';
-  protected $pw='';
-  public $table;
-  protected $pdo;
 
-  public function __construct($table)
-  {
-    $this->table=$table;
-    $this->pdo=new PDO($this->dsn,$this->user,$this->pw);
-  }
+class DB
+{
+    protected $table;
+    protected $dsn='mysql:host=localhost;charset=utf8;dbname=db19';
+    protected $pdo;
 
-  public function all(...$arg)
-  {
-    $sql="select * from $this->table ";
-    if(isset($arg[0])){
-      if(is_array($arg[0])){
-        foreach($arg[0] as $key => $value){
-          $tmp[]="`$key`='$value'";
-        }
-        $sql .=" WHERE " .join(" AND ",$tmp);
-      }else{
-        $sql .=$arg[0];
-      }
+    function __construct($table)
+    {
+        $this->table=$table;
+        $this->pdo=new PDO($this->dsn,'root','');
     }
-    if(isset($arg[1])){
-        $sql .=$arg[1];
-    }
-    return $this->pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
-  }
 
-  public function find($id)
-  {
-    $sql="select * from $this->table ";
-      if(is_array($id)){
-        foreach($id as $key => $value){
-          $tmp[]="`$key`='$value'";
+    function all(...$arg)
+    {
+        $sql="select * from $this->table ";
+        if(isset($arg[0]))
+        {
+            if(is_array($arg[0]))
+            {
+                foreach($arg[0] as $key => $val){
+                    $tmp[]="`$key`='$val'";
+                }
+    
+                $sql.= " where ".join(" && ",$tmp);
+            }else{
+                $sql.=$arg[0];
+            }
         }
-        $sql .=" WHERE " .join(" AND ",$tmp);
-      }else{
-        $sql .=" WHERE `id` = '$id' ";
-      }
-    return $this->pdo->query($sql)->fetch(PDO::FETCH_ASSOC);
-  }
 
-  public function del($id)
-  {
-    $sql="DELETE from $this->table ";
-      if(is_array($id)){
-        foreach($id as $key => $value){
-          $tmp[]="`$key`='$value'";
+        if(isset($arg[1]))
+        {
+            $sql.=$arg[1];
         }
-        $sql .=" WHERE " .join(" AND ",$tmp);
-      }else{
-        $sql .=" WHERE `id` = '$id' ";
-      }
-    return $this->pdo->exec($sql);
-  }
 
-  public function save($array)
-  {
-    if(isset($array['id'])){
-      foreach($array as $key => $value){
-        if($key!='id'){
-          $tmp[]="`$key`='$value'";
-        }
-      }
-      $sql="UPDATE $this->table SET ".join(',',$tmp)." WHERE `id` = '{$array['id']}'";
-    }else{
-      $sql="INSERT INTO $this->table (`".join("`,`",array_keys($array))."`) values('".join("','",$array)."')";
+        return $this->pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
     }
-    // echo $sql;
-    return $this->pdo->exec($sql);
-  }
 
-  public function math($math,$col,...$arg)
-  {
-    $sql="select $math($col) from $this->table ";
-    if(isset($arg[0])){
-      if(is_array($arg[0])){
-        foreach($arg[0] as $key => $value){
-          $tmp[]="`$key`='$value'";
+    function find($id)
+    {
+        $sql="select * from $this->table where ";
+        if(is_array($id))
+        {
+            foreach($id as $key => $val){
+                $tmp[]="`$key`='$val'";
+            }
+
+            $sql.= join(" && ",$tmp);
+
+        }else{
+
+            $sql.=" `id`='$id'";
         }
-        $sql .=" WHERE " .join(" AND ",$tmp);
-      }else{
-        $sql .=$arg[0];
-      }
+
+
+        return $this->pdo->query($sql)->fetch(PDO::FETCH_ASSOC);
     }
-    if(isset($arg[1])){
-        $sql .=$arg[1];
+
+    function math($math,$col,...$arg)
+    {
+        $sql="select $math($col) from $this->table ";
+
+        return $this->pdo->query($sql)->fetchColumn();
     }
-    return $this->pdo->query($sql)->fetchColumn();
-  }
-  
-  public function q($sql)
-  {
-    return $this->pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
-  }
+
+    function save($array)
+    {
+        if(isset($array['id']))
+        {
+            foreach($array as $key => $val){
+                $tmp[]="`$key`='$val'";
+            }
+
+            $sql="update $this->table set ".join(",",$tmp) . " where `id`='{$array['id']}'";
+        }else{
+
+            $col=join("`,`",array_keys($array));
+            $values=join("','",$array);
+
+            $sql="insert into $this->table (`{$col}`) values('{$values}')";
+        }
+        echo $sql;
+        return $this->pdo->exec($sql);
+    }
+
+    function del($id)
+    {
+        $sql="delete from $this->table where ";
+        if(is_array($id))
+        {
+            foreach($id as $key => $val){
+                $tmp[]="`$key`='$val'";
+            }
+
+            $sql.= join(" && ",$tmp);
+
+        }else{
+
+            $sql.=" `id`='$id'";
+        }
+
+        return $this->pdo->exec($sql);
+    }
+    
+    function q($sql)
+    {
+        return $this->pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
+    }
 
 }
 
-function to($url)
-{
-  header("location:".$url);
+function dd($array){
+    echo "<pre>";
+    print_r($array);
+    echo "</pre>";
 }
 
-function dd($array)
-{
-  echo "<pre>";
-  print_r($array);
-  echo "</pre>";
+function to($url){
+    header("location:".$url);
 }
 
 $Poster=new DB('poster');
